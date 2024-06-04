9b659f28ae652f854618b266efa1772cd35340a3
AroraKaran19 <karora0319@gmail.com>
2024-06-05 04:16:40 +0530

    minor changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,8 +4,8 @@ function Home() {
 	return (
 		<div className="h-[150vh] bg-[#191919]">
 			<div className="home-first-container h-auto w-full" id="start">
-				<div className="home-first-content flex h-full w-full">
-					<p id="title" className="text-white sm:w-full sm:text-center">
+				<div className="home-first-content flex h-full w-full sm:flex-col justify-center items-center">
+					<p id="title" className="text-white w-[50%] text-center">
 						<span>YOUR</span>
 						<span>PARTNER</span>
 						<span>IN</span>
